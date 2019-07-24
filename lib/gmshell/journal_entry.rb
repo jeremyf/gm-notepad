0d2cd90a4f2986ea21ba3f88937e4c7cd82a51aa
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2019-07-24 12:30:03 -0400

    Refining class names

--- lib/gmshell/journal_entry.rb
+++ lib/gmshell/journal_entry.rb
@@ -1,20 +1,22 @@
 require 'time'
 module Gmshell
   class JournalEntry
-    def initialize(config)
+    def initialize(**config)
       self.config = config
+      self.io = config.fetch(:io) { $stdout }
+      self.logger = config.fetch(:logger) { $stderr }
       self.term_registry = config.fetch(:term_registry) { default_term_registry }
-      self.term_expander = config.fetch(:term_expander) { default_term_expander }
+      self.line_evaluator = config.fetch(:line_evaluator) { default_line_evaluator }
       @lines = []
     end
 
-    def add(line:)
-      line = term_expander.expand(line: line, term_registry: term_registry)
-      $stdout.puts("=>\t#{line}")
+    def write(line:)
+      evaluated_line = line_evaluator.call(line: line.to_s.strip, term_registry: term_registry)
+      logger.puts("=>\t#{evaluated_line}")
       if timestamp?
-        @lines << "#{Time.now}\t#{line}"
+        @lines << "#{Time.now}\t#{evaluated_line}"
       else
-        @lines << line
+        @lines << evaluated_line
       end
     end
 
@@ -26,18 +28,14 @@ module Gmshell
 
     private
 
-    attr_accessor :term_expander, :config, :term_registry
-
-    def io
-      config.fetch(:io)
-    end
+    attr_accessor :line_evaluator, :config, :term_registry, :io, :logger
 
     def timestamp?
       config[:timestamp]
     end
 
-    def default_term_expander
-      TermExpander.new
+    def default_line_evaluator
+      LineEvaluator
     end
 
     def default_term_registry
@@ -45,9 +43,9 @@ module Gmshell
     end
   end
 
-  class TermExpander
+  module LineEvaluator
     TOKEN_REGEXP = %r{(?<term_container>\{(?<term>[^\}]+)\})}
-    def expand(line:, term_registry:)
+    def self.call(line:, term_registry:)
       while match = line.match(TOKEN_REGEXP)
         evaluated_term = term_registry.evaluate(term: match[:term])
         line.sub!(match[:term_container], evaluated_term)
