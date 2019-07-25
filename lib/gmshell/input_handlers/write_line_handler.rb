3d5a7b6c6294c486085691c7cc8b4092e9fcf3f4
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2019-07-25 19:22:01 -0400

    Updating to auto-expand or not

--- lib/gmshell/input_handlers/write_line_handler.rb
+++ lib/gmshell/input_handlers/write_line_handler.rb
@@ -8,25 +8,22 @@ module Gmshell
         true
       end
 
+      attr_accessor :line
       def after_initialize!
         self.expand_line = false
         self.to_output = true
         self.to_interactive = true
-      end
-
-      def to_params
-        line = input
-        if line[0] == NON_EXPANDING_CHARATER
+        if input[0] == NON_EXPANDING_CHARATER
+          self.line = input[1..-1].strip
           self.expand_line = false
-          { line: line[1..-1].strip, expand_line: false, to_output: true }
         else
+          self.line = input.strip
           self.expand_line = true
-          { line: line.strip, expand_line: true, to_output: true }
         end
       end
 
       def lines(**kwargs)
-        [input]
+        [line]
       end
     end
   end
--- spec/lib/gmshell/input_handlers/write_line_handler_spec.rb
+++ spec/lib/gmshell/input_handlers/write_line_handler_spec.rb
@@ -4,11 +4,19 @@ require 'gmshell/input_handlers/write_line_handler'
 module Gmshell
   module InputHandlers
     RSpec.describe WriteLineHandler do
-      let(:handler) { described_class.new(input: nil) }
+      let(:input) { "" }
+      let(:handler) { described_class.new(input: input) }
       subject { handler }
       its(:to_interactive) { is_expected.to be_truthy }
       its(:to_output) { is_expected.to be_truthy }
-      its(:expand_line?) { is_expected.to be_falsey }
+      context "when input starts with !" do
+        let(:input) { "!" }
+        its(:expand_line?) { is_expected.to be_falsey }
+      end
+      context "when input starts with !" do
+        let(:input) { "" }
+        its(:expand_line?) { is_expected.to be_truthy }
+      end
     end
   end
 end
