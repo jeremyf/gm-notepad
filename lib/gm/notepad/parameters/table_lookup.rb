ff0ac4d0ea3f5410f5da3373d7ee4dc924d8685b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2019-07-27 17:07:25 -0400

    Add handling for cells in the parameters
    Still not connected on the find. Likely will require a bit more
    refactoring.

--- lib/gm/notepad/parameters/table_lookup.rb
+++ lib/gm/notepad/parameters/table_lookup.rb
@@ -3,26 +3,27 @@ module Gm
     module Parameters
       # Responsible for teasing apart the table logic
       class TableLookup
-        WITH_GREP_REGEXP = %r{(?<declaration>\/(?<grep>[^\/]+)/)}
-        WITH_INDEX_REGEXP = %r{(?<declaration>\[(?<index>[^\]]+)\])}
-        WITH_EMPTY_INDEX_REGEX = %r{(?<declaration>\[\])}
-        WITH_EMPTY_GREP_REGEX = %r{(?<declaration>\/\/)}
-
         def initialize(text:)
           @text = text
           extract_parameters!
         end
 
-        attr_accessor :index, :grep, :table_name
+        attr_accessor :cell, :index, :grep, :table_name
 
         def parameters
           parameters = { table_name: table_name }
           parameters[:grep] = grep if grep
           parameters[:index] = index if index
+          parameters[:cell] = cell if cell
           parameters
         end
 
         private
+        WITH_GREP_REGEXP = %r{(?<declaration>\/(?<found>[^\/]+)/)}
+        WITH_INDEX_REGEXP = %r{(?<declaration>\[(?<found>[^\]]+)\])}
+        WITH_EMPTY_INDEX_REGEX = %r{(?<declaration>\[\])}
+        WITH_EMPTY_GREP_REGEX = %r{(?<declaration>\/\/)}
+
         def extract_parameters!
           @parameters = {}
           text = @text
@@ -30,12 +31,19 @@ module Gm
             text = text.sub(match[:declaration], '')
           elsif match = WITH_INDEX_REGEXP.match(text)
             text = text.sub(match[:declaration], '')
-            @index = match[:index]
+            @index = match[:found]
+            # Moving on to the cell
+            if match = WITH_EMPTY_INDEX_REGEX.match(text)
+              text = text.sub(match[:declaration], '')
+            elsif match = WITH_INDEX_REGEXP.match(text)
+              text = text.sub(match[:declaration], '')
+              @cell = match[:found]
+            end
           elsif match = WITH_EMPTY_GREP_REGEX.match(text)
             text = text.sub(match[:declaration], '')
           elsif match = WITH_GREP_REGEXP.match(text)
             text = text.sub(match[:declaration], '')
-            @grep  =  match[:grep]
+            @grep  =  match[:found]
           end
           @table_name = text.downcase
         end
